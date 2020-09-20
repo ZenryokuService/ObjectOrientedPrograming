54eba6edd011f86cb5aef12e8949b695b5c443ff
ZenryokuService <tak4545@outlook.jp>
2020-09-20 14:04:33 +0900

    Lv1 Hello Worldの実装

---
+++ src/main/java/jp/zenryoku/practice/Lv1HelloWorld.java
@@ -0,0 +1,18 @@
+package jp.zenryoku.practice;
+
+/**
+ * サンプル１のフローチャートをコードにする。
+ *
+ * @author 作成者の名前
+ *
+ */
+public class Lv1HelloWorld {
+	/**
+	 * メインメソッド
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
