5deb3323adaded30ebc732aa15ce3780cc7024e6
takk <yogotak45@gmail.com>
2020-10-21 20:47:53 +0900

    継承サンプル

---
+++ src/main/java/jp/zenryoku/practice/sample/ChildTable1.java
@@ -0,0 +1,22 @@
+package jp.zenryoku.practice.sample;
+
+/** 子クラス１: test1テーブル用クラス */
+public class ChildTable1 extends Parent2{
+	/** コンストラクタ */
+	public ChildTable1() {
+		super();
+		super.tableName = "test1Table";
+	}
+
+	/** SQLの実行 */
+	public void execute() {
+		// 取得
+		select(1);
+		// 登録
+		insert(2);
+		// 更新
+		update(3);
+		// 削除
+		delete(4);
+	}
+}
---
+++ src/main/java/jp/zenryoku/practice/sample/Parent2.java
@@ -0,0 +1,58 @@
+package jp.zenryoku.practice.sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/** 親クラス */
+public abstract class Parent2 {
+	/** テーブル名 */
+	protected String tableName;
+	/** DBコネクション */
+	protected Connection con;
+
+	public Parent2() {
+		try {
+			// DBコネクションの取得
+			con = DriverManager.getConnection("DBへのURL");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/** SQLの実行メソッド */
+	protected void execute(String sql) {
+		try {
+			Statement stm = con.createStatement();
+			// SQLの実行
+			stm.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/** データの取得メソッド */
+	public void select(int id) {
+		// DBからデータを取得する処理
+		String sql = "select * from " + this.tableName + " where " + this.tableName + ".id";
+		execute(sql);
+	}
+
+	/** データの登録メソッド */
+	public void insert(int id) {
+		// DBからデータを登録する処理
+	}
+
+	/** データの更新メソッド */
+	public void update(int id) {
+		// DBからデータを更新する処理
+	}
+
+	/** データの削除メソッド */
+	public void delete(int id) {
+		// DBからデータを削除する処理
+	}
+
+}
