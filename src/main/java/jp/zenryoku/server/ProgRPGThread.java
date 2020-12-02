1f8ea821000b5ae8097fe091e95c433be64a33bf
ZenryokuService <tak4545@outlook.jp>
2020-12-02 22:05:34 +0900

    PRGサーバー側のみ作成

---
+++ src/main/java/jp/zenryoku/server/ProgRPGThread.java
@@ -0,0 +1,15 @@
+package jp.zenryoku.server;
+
+import java.net.Socket;
+
+/**
+ * プログラミングコンテストゲームのメインスレッド。
+ *
+ * @author 実装者の名前
+ */
+public class ProgRPGThread extends Thread {
+
+	public ProgRPGThread(Socket socket) {
+
+	}
+}
