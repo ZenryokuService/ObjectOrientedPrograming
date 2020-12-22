99dd20bca6d3c44db680f47f23681cac0e662a35
ZenryokuService <tak4545@outlook.jp>
2020-12-22 21:08:04 +0900

    ステータス生成、ヨガ数秘処理のテスト完了

--- src/main/java/jp/zenryoku/rpg/util/StatusUtils.java
+++ src/main/java/jp/zenryoku/rpg/util/StatusUtils.java
@@ -10,22 +10,69 @@ public class StatusUtils {
 	public static final int ERROR = 0;
 
 	/**
+	 * 生年月日から、「月」「日」「月と日の合計」「西暦の下2桁」を算出する。
+	 * 余が数秘の表を作るための要素となる数を算出する。
 	 *
-	 * @param bornDate 生年月日(yyyyMMdd)
-	 * @return ヨガ数秘の配列
+	 * @param birthDate 生年月日(yyyyMMdd)
+	 * @return ヨガ数秘の配列：[0]=月、[1]=日、[2]=月と日の合計、[3]=西暦の下2桁
 	 */
-	public static String[] createYogaSuhi(String bornDate) {
-		String[] result = new String[8];
-		result[0] = String.valueOf(StatusUtils.convertStringToInt(bornDate.substring(4,6)));
-		result[1] = String.valueOf(StatusUtils.convertStringToInt(bornDate.substring(6,8)));
-
+	public static String[] createYogaSuhi(String birthDate) {
+		String[] result = new String[9];
+		// 月を算出
+		result[0] = String.valueOf(StatusUtils.convertStringToInt(birthDate.substring(4,6)));
+		// 日を算出
+		result[1] = String.valueOf(StatusUtils.convertStringToInt(birthDate.substring(6,8)));
+		// 月と日の合計を算出
 		String v1 = String.valueOf(Integer.parseInt(result[0]) + Integer.parseInt(result[1]));
 		result[2] = String.valueOf(StatusUtils.convertStringToInt(v1));
+		// 西暦の下2桁を算出
+		result[3] = String.valueOf(StatusUtils.convertStringToInt(birthDate.substring(2, 4)));
+		// 西暦4桁
+		result[4] = StatusUtils.convertStringToInt(birthDate.substring(0, 2), birthDate.substring(2, 4));
+		// 4 + 5
+		int gokei = Integer.parseInt(result[3]) + Integer.parseInt(result[4]);
+		result [5] = String.valueOf(StatusUtils.convertStringToInt(String.valueOf(gokei)));
+		// 2 + 4
+		int gokei2 = Integer.parseInt(result[1]) + Integer.parseInt(result[3]);
+		result [6] = String.valueOf(StatusUtils.convertStringToInt(String.valueOf(gokei2)));
+		// 1 + 5
+		int gokei3 = Integer.parseInt(result[0]) + Integer.parseInt(result[4]);
+		result [7] = String.valueOf(StatusUtils.convertStringToInt(String.valueOf(gokei3)));
+		// 生年月日
+		result[8] = String.valueOf(StatusUtils.convertStringToInt(birthDate.substring(0, 4).toCharArray()));
 
-		result[3] = String.valueOf(StatusUtils.convertStringToInt(bornDate.substring(2, 4)));
+//		Map<Integer, String> suhi = new HashMap<>();
+		// 1. 誕生月
+		// 2. 誕生日
+		// 3. 1 + 2
+		// 4. 西暦の下2桁
+		// 5. 西暦4桁
+
 
 
 		return result;
+	}
+
+	/**
+	 * 西暦4桁を合計して数秘的合算を行う。
+	 * @param seireki 西暦4桁
+	 * @return
+	 */
+	public static int convertStringToInt(char[] seireki) {
+		if (4 != seireki.length) {
+			return ERROR;
+		}
+		int num = 0;
+
+		for (char ch : seireki) {
+			String str = String.valueOf(ch);
+			num += Integer.parseInt(str);
+		}
+
+		while (num > 10) {
+			num = convertStringToInt(String.valueOf(num));
+		}
+		return num;
 	}
 
 	/**
@@ -42,6 +89,29 @@ public class StatusUtils {
 		return check;
 	}
 
+	/**
+	 * 数秘術演算、２桁の数字を１桁にする
+	 * @param val 2桁の数字
+	 * @return 1桁の数、エラーは0を返す
+	 */
+	public static String convertStringToInt(String val, String val2) {
+		int num1 = convertStringToInt(val);
+		int num2 = convertStringToInt(val2);
+		int gokei = num1 + num2;
+
+		while (gokei > 10) {
+			gokei = convertStringToInt(String.valueOf(gokei));
+		}
+		return String.valueOf(gokei);
+	}
+
+	/**
+	 * 二桁の数字文字列を、分割し合計する。
+	 * 例；12 => 1 + 2 = 3
+	 *
+	 * @param val
+	 * @return 数秘的な合計値
+	 */
 	public static int checkReturnNum(String val) {
 		// 2桁である確認
 		if (val != null && val.length() != 2) {
@@ -55,4 +125,5 @@ public class StatusUtils {
 
 		return (left + right);
 	}
+
 }
--- src/test/java/jp/zenryoku/rpg/util/StatusUtilsTest.java
+++ src/test/java/jp/zenryoku/rpg/util/StatusUtilsTest.java
@@ -6,12 +6,23 @@ import org.junit.jupiter.api.Test;
 
 public class StatusUtilsTest {
 
+	/**
+	 * 誕生日の月を数秘の計算で番号を算出する処理。
+	 * <Ex></br>
+	 * 10/28であれば、「1 + 1 = 1」と「」
+	 */
 	@Test
 	public void testGetTanjoGetu() {
 		String[] yoga = StatusUtils.createYogaSuhi("19800328");
 		assertEquals("3", yoga[0]);
 		assertEquals("10", yoga[1]);
 		assertEquals("4", yoga[2]);
+		assertEquals("8", yoga[3]);
+		assertEquals("9", yoga[4]);
+		assertEquals("8", yoga[5]);
+		assertEquals("9", yoga[6]);
+		assertEquals("3", yoga[7]);
+		assertEquals("9", yoga[8]);
 	}
 
 
@@ -23,6 +34,11 @@ public class StatusUtilsTest {
 		assertEquals(2, StatusUtils.convertStringToInt("29"));
 		assertEquals(3, StatusUtils.convertStringToInt("03"));
 		assertEquals(10, StatusUtils.convertStringToInt("28"));
+		assertEquals(8, StatusUtils.convertStringToInt("80"));
+	}
+
+	@Test
+	public void testSuhiMap() {
 
 	}
 }
