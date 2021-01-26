847034988caa4a7cd10d125745bad47d8a7af79a
余湖 巧 <tkm-yogo@yoshida-g.jp>
2021-01-26 17:31:36 +0900

    チェックディジット実装

--- src/test/java/jp/zenryoku/rpg/util/CheckerUtilsTest.java
+++ src/test/java/jp/zenryoku/rpg/util/CheckerUtilsTest.java
@@ -40,4 +40,25 @@ public class CheckerUtilsTest {
 		assertTrue(CheckerUtils.isCommandInput("3", "[1-3]"));
 		assertFalse(CheckerUtils.isCommandInput("0", "[1-3]"));
 	}
+
+	@Test
+	public void testCHeckDigit() {
+		assertEquals(3, CheckerUtils.createCheckDigit("201"));
+		assertEquals(5, CheckerUtils.createCheckDigit("2012"));
+		assertEquals(4, CheckerUtils.createCheckDigit("20129"));
+		assertEquals(7, CheckerUtils.createCheckDigit("201293"));
+
+		// チェックディジット付きの注文番号生成
+		String tyumonNo = "201293";
+		String digit = CheckerUtils.createCheckDigit(tyumonNo).toString();
+		String data = tyumonNo + digit;
+
+		// チェックディジットを使用してチェックする
+
+		// 最後の一文字を取得
+		String checkDigit = data.substring(data.length() - 1);
+		System.out.println("Digit: " + checkDigit);
+		assertTrue("7".equals(checkDigit));
+
+	}
 }
