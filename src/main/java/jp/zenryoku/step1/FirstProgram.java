b69975ad36563e2a6f83d02accbecbec208d9584
Takunoji <tak4545@outlook.jp>
2022-02-04 14:13:03 +0900

    Add Challenge1-5

--- src/main/java/jp/zenryoku/step1/FirstProgram.java
+++ src/main/java/jp/zenryoku/step1/FirstProgram.java
@@ -60,4 +60,21 @@ public class FirstProgram {
 		System.out.println("   *");
 	}
 
+	/**
+	 * ハローワールドが動かせたら、「＊(アスタリスク)」を使用して
+	 * 簡単な絵をかいてみましょう
+	 * 例えば大文字焼を描く
+	 */
+	public static void firstChallenge5() {
+		System.out.println("                **");
+		System.out.println("              **  **");
+		System.out.println("            **      **");
+		System.out.println("          **     *     **");
+		System.out.println("        **   * * * * *   **");
+		System.out.println("      **         *         **");
+		System.out.println("    **         *   *         **");
+		System.out.println(" **         *         *         **");
+		System.out.println("** * ** ** ** * * * ** * ** ** * **");
+	}
+
 }
