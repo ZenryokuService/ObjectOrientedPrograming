705ddc403d025f353eb549b27e5e8cddd7ad04fb
Takunoji <tak4545@outlook.jp>
2022-02-04 16:07:11 +0900

    Add sample of operators and question.

--- src/main/java/jp/zenryoku/step1/FirstProgram.java
+++ src/main/java/jp/zenryoku/step1/FirstProgram.java
@@ -77,4 +77,28 @@ public class FirstProgram {
 		System.out.println("** * ** ** ** * * * ** * ** ** * **");
 	}
 
+	/**
+	 * 「+」演算子を使用してみよう。
+	 * 「+」演算子は、文字列と文字列をくっつけます。
+	 * 他の「四則演算」の演算子は、計算で使用します。
+	 */
+	public static void usePlusOperator() {
+		System.out.println("文字列と文字列を連結します。");
+		// "abc" + "def": 「"」で囲っていると文字列になります。
+		System.out.println("abc" + "def" + "ghi");
+		// "abc" + 1: 文字列に整数(INT型)を足します。
+		System.out.println("abc" + 1);
+		// "abc" + (1 + 2): 文字列に整数(INT型)を足し算。「()」で処理の順序を指定できます。
+		System.out.println("abc" + (1 + 2));
+		// "def" + (3 - 1): 文字列に整数(INT型)を引き算します。「()」で処理の順序を指定できます。
+		System.out.println("def" + (3 - 1));
+		// "ghi" + (4 * 2): 文字列に整数(INT型)を掛け算します。「()」で処理の順序を指定できます。
+		System.out.println("def" + (4 * 2));
+		// "jkl" + (4 / 2): 文字列に整数(INT型)を割り算します。「()」で処理の順序を指定できます。
+		System.out.println("jkl" + (6 / 2));
+
+		// Question1 :「3 掛ける 4 足す 12」をプログラムで書いてください。
+		// Question2 :「1 引く 4 割る 3」をプログラムで書いてください。
+		// Question3 :「1辺が4cmの正三角形の面積を計算して「～cm」と表示するプログラムを書いてください。
+	}
 }
