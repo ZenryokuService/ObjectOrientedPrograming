83b85ee898481d0e85c6a05d6144158f10057cf0
Takunoji <tak4545@outlook.jp>
2022-02-04 12:04:29 +0900

    Add first Lesson code
    ハローワールドから、簡単な絵を描く

---
+++ src/main/java/jp/zenryoku/step1/FirstProgram.java
@@ -0,0 +1,63 @@
+package steps.java.step1;
+
+/**
+ * 初めのプログラム「Hello World」の実装。
+ * そして、簡単な絵をかいてみる。
+ *
+ */
+public class FirstProgram {
+	/** メインメソッド */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	/**
+	 * ハローワールドが動かせたら、「＊(アスタリスク)」を使用して
+	 * 簡単な絵をかいてみましょう
+	 * 例えば四角を描く
+	 */
+	public static void firstChallenge() {
+		System.out.println("*****");
+		System.out.println("*   *");
+		System.out.println("*   *");
+		System.out.println("*****");
+	}
+
+	/**
+	 * ハローワールドが動かせたら、「＊(アスタリスク)」を使用して
+	 * 簡単な絵をかいてみましょう
+	 * 例えば三角を描く
+	 */
+	public static void firstChallenge2() {
+		System.out.println("   *");
+		System.out.println("  * *");
+		System.out.println(" *   *");
+		System.out.println("*******");
+	}
+
+	/**
+	 * ハローワールドが動かせたら、「＊(アスタリスク)」を使用して
+	 * 簡単な絵をかいてみましょう
+	 * 例えば逆三角を描く
+	 */
+	public static void firstChallenge3() {
+		System.out.println("*******");
+		System.out.println(" *   *");
+		System.out.println("  * *");
+		System.out.println("   *");
+	}
+
+	/**
+	 * ハローワールドが動かせたら、「＊(アスタリスク)」を使用して
+	 * 簡単な絵をかいてみましょう
+	 * 例えばひし形を描く
+	 */
+	public static void firstChallenge4() {
+		System.out.println("   * ");
+		System.out.println(" *   *");
+		System.out.println("*     *");
+		System.out.println(" *   *");
+		System.out.println("   *");
+	}
+
+}
