ff7fdc3cfc92323c1178de236f240b4b77eeae6b
ZenryokuService <tak4545@outlook.jp>
2022-03-27 19:52:08 +0900

    チェッカーバッチ作成開始

---
+++ src/main/java/jp/zenryoku/rpg/bat/StoryTextChecker.java
@@ -0,0 +1,63 @@
+package jp.zenryoku.rpg.bat;
+
+import jp.zenryoku.rpg.constants.MessageConst;
+import jp.zenryoku.rpg.exception.RpgException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * ストーリーテキスト(Story.txt)のチェック処理を行う。
+ */
+public class StoryTextChecker {
+    /** ファイルまでのパス */
+    private String path;
+    /** チェック対象フォルダ */
+    private String directory;
+    private final String TXT_FILE = "Story.txt";
+
+    /**
+     * コンストラクタ。固定のチェック対象ファイルのディレクトリを指定する。
+     * @throws RpgException
+     */
+    public StoryTextChecker() throws RpgException {
+        path = "src/main/resources/" + TXT_FILE;
+        BufferedReader buf = getBufferedReader();
+        // チェック開始
+        execute(buf);
+    }
+
+    /**
+     * コンストラクタ。固定のチェック対象ファイルのディレクトリを指定する。
+     * @throws RpgException
+     */
+    public StoryTextChecker(String path) throws RpgException {
+        this.path = path + TXT_FILE;
+        BufferedReader buf = getBufferedReader();
+        // チェック開始
+        execute(buf);
+    }
+
+    /**
+     * BufferedReaderを取得する。
+     * @throws RpgException
+     */
+    private BufferedReader getBufferedReader() throws RpgException {
+        BufferedReader buf = null;
+        try {
+            Path p = Paths.get(path);
+            buf = Files.newBufferedReader(p);
+        } catch (IOException ie) {
+            ie.printStackTrace();
+            throw new RpgException(MessageConst.ERR_FILE_READ.toString());
+        }
+        return buf;
+    }
+
+    public void execute(BufferedReader buf) {
+
+    }
+}
